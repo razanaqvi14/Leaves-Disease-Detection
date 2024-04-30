bd0c0f4cc7cfa30a7a8441a66bd1b62920cdc9ca
razanaqvi <razanaqvi2004@gmail.com>
2024-05-01 00:04:39 +0500

    code optimization and ui improvement

--- main.py
+++ main.py
@@ -4,14 +4,28 @@ import io
 from PIL import Image
 import tensorflow as tf
 
+page_title = "Potato Leaf Health Assessment System"
+page_icon = ":potato:"
+layout = "centered"
+
+accuracy_of_model = 91.66
+
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+
+st.title("Welcome to :orange[Potato] :green[Leaf] Health Assessment System")
+
+st.divider()
+
+st.info(
+    f"Note: This model is {accuracy_of_model}% accurate so it may make mistakes, consider confirming the information.\n\nThings to remember:\n - The image should contain a leaf, and the leaf itself should belong to a potato; otherwise, the model will not be able to assess that leaf correctly.\n - Before reuploading, please remove the uploaded image by clicking on the cross on the right side of the image's name or it will give errors.\n - If you reload the page, the uploaded image and prediction will be lost and you have to reupload the image to assess the leaf."
+)
+
 
 def load_model():
     return tf.keras.models.load_model("./models/model.keras")
 
 
 model = load_model()
-
-accuracy_of_model = 91.66
 
 class_names = ["Early blight", "Late blight", "healthy"]
 
@@ -20,22 +34,14 @@ def predict_leaf_class(image_as_array):
     try:
         prediction = model.predict(image_as_array[np.newaxis, ...])
         if class_names[np.argmax(prediction)] == "healthy":
-            st.markdown(f"#### The above leaf is healthy")
+            st.success("The above leaf is healthy")
         else:
-            st.markdown(
-                f"#### The above leaf is suffering from {class_names[np.argmax(prediction)]}"
+            st.error(
+                f"The above leaf is suffering from {class_names[np.argmax(prediction)]}"
             )
     except Exception as e:
         st.error(f"An error occurred during prediction: {e}")
 
-
-st.title("Welcome To :green[Leaf] Disease Predictor")
-
-st.divider()
-
-st.info(
-    f"Note: This model is {accuracy_of_model}% accurate so it may make mistakes, consider confirming the information."
-)
 
 uploaded_file = st.file_uploader(
     "Drag a :leaves: image here or upload",
@@ -47,9 +53,7 @@ if uploaded_file is not None:
         image = Image.open(io.BytesIO(uploaded_file.getvalue()))
         image_as_array = np.array(image)
         st.image(uploaded_file)
-        predict_leaf_class(image_as_array)
-        st.warning(
-            "Warning: If you reload the page, the uploaded image and prediction will disappear and then you have to reupload the image again to get the prediction."
-        )
+        if st.button("Detect", type="primary"):
+            predict_leaf_class(image_as_array)
     except Exception as e:
         st.error(f"An error occurred: {e}")
